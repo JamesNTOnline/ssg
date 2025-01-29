60b3672b749c147403e0d78cb93a608b8089fc7d
JamesNTOnline <james.nicholas.thomas@gmail.com>
2025-01-29 05:50:08 +0000

    markdown to document conversion complete with tests

--- src/node_converter.py
+++ src/node_converter.py
@@ -7,6 +7,13 @@ import re
 
 
 def text_node_to_html_node(text_node):
+    """
+    converts a TextNode to a LeafNode (HTMLNode)
+    args:
+        text_node (TextNode): a TextNode object
+    returns:
+        LeafNode: a LeafNode object
+    """
     if text_node.text_type == TextType.TEXT:
         return LeafNode(None, text_node.text)
     if text_node.text_type == TextType.BOLD:
@@ -25,7 +32,15 @@ def text_node_to_html_node(text_node):
     raise ValueError("Unknown text type")
 
 
+
 def text_to_textnodes(text):
+    """
+    Splits a text into text nodes based on inline markdown formatting
+    Args:
+        text (str): a piece of markdown text to split into nodes
+    Returns:
+        list: list of TextNodes.
+    """
     new_nodes = [TextNode(text, TextType.TEXT)]
     delimiters = [("**", TextType.BOLD), ("*", TextType.ITALIC), ("`", TextType.CODE)]
     for d in delimiters:
@@ -54,27 +69,58 @@ def block_to_block_type(text):
     return "paragraph"
     
     
-def block_to_html_node(markdown):
+def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
-    
+    page_nodes = []
     for block in blocks:
         block_type = block_to_block_type(block)
+        if block_type == "paragraph":
+            page_nodes.append(paragraph_to_html_node(block))
+        if block_type == "heading":
+            page_nodes.append(header_block_to_html_node(block))
+        if block_type == "code":
+            page_nodes.append(code_block_to_html_node(block))
+        if block_type == "quote":
+            page_nodes.append(quote_block_to_html_node(block))
+        if block_type == "ordered list" or block_type == "unordered list":
+            page_nodes.append(list_block_to_html_nodes(block))
+    return ParentNode("div", page_nodes)
 
+def paragraph_to_html_node(block):
+    child_nodes = text_to_children(block)
+    return ParentNode("p", child_nodes)
+
+def header_block_to_html_node(block):
+    header_md = block.split(" ", 1)
+    level = len(header_md[0]) if len(header_md[0]) <= 6 else 6
+    contents = header_md[1]
+    child_nodes = text_to_children(contents)    
+    return ParentNode(f"h{level}", child_nodes)
+
+def code_block_to_html_node(block):
+    code = block[3:-3]
+    code_node = LeafNode("code", code)
+    return ParentNode("pre", [code_node])
+
+def quote_block_to_html_node(block):
+    contents = block.replace("\n>", "\n").lstrip(">")
+    child_nodes = text_to_children(contents)
+    return ParentNode("blockquote", child_nodes)
 
 def list_block_to_html_nodes(block):
     tag = "ul" if block.startswith("*") else "ol"
     list_nodes = []
     lines = block.split("\n")
-    items = [line[2:] for line in lines]
-    for item in items:
-        child_nodes = []
-        text_nodes = text_to_textnodes(item)
-        for node in text_nodes:
-            child_nodes.append(text_node_to_html_node(node))
+    # splits each line into its markdown and content, then strips the content in [1] to use for the list of items
+    contents = [line.split(" ", 1)[1].strip() for line in lines]
+    for item in contents:
+        child_nodes = text_to_children(item)
         list_nodes.append(ParentNode("li", child_nodes))
     return ParentNode(tag, list_nodes)
 
-
-def text_to_child_nodes(text):
+def text_to_children(text):
     text_nodes = text_to_textnodes(text)
-    return [text_node_to_html_node(node) for node in text_nodes]
+    children = []
+    for node in text_nodes:
+        children.append(text_node_to_html_node(node))
+    return children
--- tests/test_node_converter.py
+++ tests/test_node_converter.py
@@ -39,29 +39,207 @@ class TestNodeConverter(unittest.TestCase):
         self.assertEqual(expected, actual)
     
     
-    # tests for blocks to html nodes 
-    def test_block_to_html_node_list(self):
-        text = "* This is a test\n* This is **bold** text\n* Here's *italic* text"
+    # testing paragraph md to html conversion
+    def test_paragraph_block_to_html_node(self):
+        text = "This is a paragraph"
+        expected = ParentNode("p", [
+            LeafNode(None, "This is a paragraph")
+        ]).to_html()
+        actual = paragraph_to_html_node(text).to_html()
+        self.assertEqual(expected, actual)
+    
+    def test_paragraph_with_formatted_text(self):
+        text = "This is a paragraph with **bold text** and *italic words*"
+        expected = ParentNode("p", [
+            LeafNode(None, "This is a paragraph with "),
+            ParentNode("b", [
+                LeafNode(None, "bold text")
+            ]),
+            LeafNode(None, " and "),
+            ParentNode("i", [
+                LeafNode(None, "italic words")
+            ])
+        ]).to_html()
+        actual = paragraph_to_html_node(text).to_html()
+        self.assertEqual(expected, actual)
+    
+    # testing header md to html conversion
+    def test_header_block_to_html_node(self):
+        text = "# Heading"
+        expected = ParentNode("h1", [
+            LeafNode(None, "Heading")
+        ]).to_html()
+        actual = header_block_to_html_node(text).to_html()
+        self.assertEqual(expected, actual)
+    
+    def test_header_with_formatted_text(self):
+        text = "## Heading with **bold text** and *italic words*"
+        expected = ParentNode("h2", [
+            LeafNode(None, "Heading with "),
+            ParentNode("b", [
+                LeafNode(None, "bold text")
+            ]),
+            LeafNode(None, " and "),
+            ParentNode("i", [
+                LeafNode(None, "italic words")
+            ])
+        ]).to_html()
+        actual = header_block_to_html_node(text).to_html()
+        self.assertEqual(expected, actual)
+    
+    # testing list md to html list conversion 
+    def test_ul_block_to_html_node(self):
+        text = "* First item\n* Second item\n* Third item"
         expected = ParentNode("ul", [
             ParentNode("li", [
-                LeafNode(None, "This is a test")
+                LeafNode(None, "First item")
             ]),
             ParentNode("li", [
-                LeafNode(None, "This is "),
-                ParentNode("b", [
-                    LeafNode(None, "bold")
-                ]),
-                LeafNode(None, " text")
+                LeafNode(None, "Second item")
             ]),
             ParentNode("li", [
-                LeafNode(None, "Here's "),
-                ParentNode("i", [
-                    LeafNode(None, "italic")
-                ]),
-                LeafNode(None, " text")
+                LeafNode(None, "Third item")
             ])
         ]).to_html()
         actual = list_block_to_html_nodes(text).to_html()
+        self.assertEqual(expected, actual)
+        
+    def test_ul_block_with_empty_items(self):
+        text = "* First item\n* \n* Third item"
+        expected = ParentNode("ul", [
+            ParentNode("li", [
+                LeafNode(None, "First item")
+            ]),
+            ParentNode("li", [
+                LeafNode(None, "")
+            ]),
+            ParentNode("li", [
+                LeafNode(None, "Third item")
+            ])
+        ]).to_html()
+        actual = list_block_to_html_nodes(text).to_html()
+        self.assertEqual(expected, actual)
+        
+    def test_list_with_formatted_text(self):
+        text = "* Item with **bold text**\n* Item with *italic words*"
+        expected = ParentNode("ul", [
+            ParentNode("li", [
+                LeafNode(None, "Item with "),
+                ParentNode("b", [
+                    LeafNode(None, "bold text")
+                ])
+            ]),
+            ParentNode("li", [
+                LeafNode(None, "Item with "),
+                ParentNode("i", [
+                    LeafNode(None, "italic words")
+                ])
+            ])
+        ]).to_html()
+        actual = list_block_to_html_nodes(text).to_html()
+        self.assertEqual(expected, actual)
+        
+    def test_ol_block_to_html_node(self):
+        text = "1. First item\n2. Second item\n3. Third item"
+        expected = ParentNode("ol", [
+            ParentNode("li", [
+                LeafNode(None, "First item")
+            ]),
+            ParentNode("li", [
+                LeafNode(None, "Second item")
+            ]),
+            ParentNode("li", [
+                LeafNode(None, "Third item")
+            ])
+        ]).to_html()
+        actual = list_block_to_html_nodes(text).to_html()
+        self.assertEqual(expected, actual)
+        
+    def test_ol_block_with_two_digit_numbers(self):
+        text = "9. Ninth\n10. Tenth\n11. Eleventh"
+        expected = ParentNode("ol", [
+            ParentNode("li", [
+                LeafNode(None, "Ninth")
+            ]),
+            ParentNode("li", [
+                LeafNode(None, "Tenth")
+            ]),
+            ParentNode("li", [
+                LeafNode(None, "Eleventh")
+            ])
+        ]).to_html()
+        actual = list_block_to_html_nodes(text).to_html()
+        self.assertEqual(expected, actual)
+
+    # testing quote md to html conversion
+    def test_quote_block_to_html(self):
+        text = ">First line\n>Second line"
+        expected = ParentNode("blockquote", [
+            LeafNode(None, "First line\nSecond line")
+        ]).to_html()
+        actual = quote_block_to_html_node(text).to_html()
+        self.assertEqual(expected, actual)
+
+    def test_quote_block_with_formatted_text(self):
+        text = ">First line with **bold text**\n>Second line with *italic words*"
+        expected = ParentNode("blockquote", [
+            LeafNode(None, "First line with "),
+            ParentNode("b", [
+                LeafNode(None, "bold text")
+            ]),
+            LeafNode(None, "\nSecond line with "),
+            ParentNode("i", [
+                LeafNode(None, "italic words")
+            ])
+        ]).to_html()
+        actual = quote_block_to_html_node(text).to_html()
+        self.assertEqual(expected, actual)
+    
+    # testing code md to html conversion
+    def test_code_block_to_html_node(self):
+        text = "```\ndef hello():\n    print('Hello!')\n```"
+        expected = ParentNode("pre", 
+            [LeafNode("code", "\ndef hello():\n    print('Hello!')\n")]).to_html()
+        actual = code_block_to_html_node(text).to_html()
+        self.assertEqual(expected, actual)
+    
+    def test_code_block_with_markdown_characters(self):
+        text = "```\ndef hello():\n    print('*italic text*')\n    return '**bold text**'\n```"
+        expected = ParentNode("pre", 
+            [LeafNode("code", "\ndef hello():\n    print('*italic text*')\n    return '**bold text**'\n")]).to_html()
+        actual = code_block_to_html_node(text).to_html()
+        self.assertEqual(expected, actual)
+    
+    # testing markdown to html conversion
+    def test_markdown_to_html_node_with_formatting_and_image(self):
+        markdown = (
+            "# Heading\n\n"
+            "This is a **bold** paragraph with an ![An image](image.png)\n\n"
+            "* List item 1\n"
+            "* List item 2"
+        )
+        expected = ParentNode("div", [
+            ParentNode("h1", [
+                LeafNode(None, "Heading")
+            ]),
+            ParentNode("p", [
+                LeafNode(None, "This is a "),
+                ParentNode("b", [
+                    LeafNode(None, "bold")
+                ]),
+                LeafNode(None, " paragraph with an "),
+                LeafNode("img", "", {"src": "image.png", "alt": "An image"})
+            ]),
+            ParentNode("ul", [
+                ParentNode("li", [
+                    LeafNode(None, "List item 1")
+                ]),
+                ParentNode("li", [
+                    LeafNode(None, "List item 2")
+                ])
+            ])
+        ]).to_html()
+        actual = markdown_to_html_node(markdown).to_html()
         self.assertEqual(expected, actual)
     
     # block to type testing
