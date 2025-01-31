765dceb647a09c1a73569f78af36f25f03989832
JamesNTOnline <james.nicholas.thomas@gmail.com>
2025-01-31 22:52:31 +0000

    setting up for site creation

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from textnode import TextNode, TextType
+import os
 
+def copy_static(origin, destination):
+    print("Copying static folder files")
+    
+    
 
 def main():
     node_one = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node_one)
+
+
 
 if __name__ == "__main__":
     main()
