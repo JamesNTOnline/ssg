abff40e2392cc389902fd5f570498619f4cad650
JamesNTOnline <james.nicholas.thomas@gmail.com>
2025-02-03 21:59:45 +0000

    copy static function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 from textnode import TextNode, TextType
 import os
 
-def copy_static(origin, destination):
-    print("Copying static folder files")
+def copy_static(src_dir, dest_dir):
+    if not os.path.exists(src_dir):
+        raise Exception(f"Source directory '{src_dir}' does not exist")
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+    contents = os.listdir(src_dir)
+    for item in contents:
+        src_path = os.path.join(src_dir, item)
+        dest_path = os.path.join(dest_dir, item)
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        else:
+            #os.mkdir(dest_path)
+            copy_static(src_path, dest_path)
     
     
 
